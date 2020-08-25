7507c3da088b9fbaaa0aa6e5c0db6b6f6a6d9d7e
Julius Pedersen <deifyed@ctemplar.com>
2020-08-25 10:49:28 +0200

    👌 Improve project structure by creating a pypi package of common libs

---
+++ harvest/harvester/aws/aws.py
@@ -0,0 +1,42 @@
+import json
+import os
+
+import boto3
+from botocore.config import Config
+
+from origo.devportal.poctools.models import API, Source
+
+config = Config(
+    region_name='eu-north-1',
+    signature_version='v4',
+    retries={
+        'max_attempts': 10,
+        'mode': 'standard'
+    }
+)
+
+credentials = {
+    'aws_access_key_id': os.environ['AWS_ACCESS_KEY_ID'],
+    'aws_secret_access_key': os.environ['AWS_SECRET_ACCESS_KEY']
+}
+
+source = Source.loadFromEnv()
+
+clientv1 = boto3.client('apigateway', **credentials, config=config)
+clientv2 = boto3.client('apigatewayv2', **credentials, config=config)
+
+response_v1 = clientv1.get_rest_apis()
+response_v2 = clientv2.get_apis()
+
+apis = list()
+for raw_api in response_v1['items']:
+    api = API(raw_api['name'], source.identifier)
+
+    apis.append(api.serialize())
+
+for raw_api in response_v2['Items']:
+    api = API(raw_api['Name'], source.identifier)
+
+    apis.append(api.serialize())
+
+print(json.dumps(apis))
--- lib/models/creator.py
+++
@@ -1,7 +0,0 @@
-class Creator():
-    @property
-    def name(self, name):
-        return self._name
-    @name.setter
-    def name(self, name):
-        self._name = name
