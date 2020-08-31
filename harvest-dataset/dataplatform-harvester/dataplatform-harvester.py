1c325a98d98e8785e4c772655a78d0e6092d5434
Yngvar Kristiansen <yngkri@knowit.no>
2020-08-31 16:17:50 +0200

    #16: ✅ Add working dataset harvester

---
+++ harvest-dataset/dataplatform-harvester/dataplatform-harvester.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+DATASETS_BASE_URL = os.environ['DATASET_API_BASE_URL']
+
+
+def get_datasets():
+    headers = {'accept': 'application/json'}
+    r = requests.get(f"{DATASETS_BASE_URL}/metadata/datasets", headers=headers)
+
+    if r.status_code != 200:
+        raise Exception(f"Got unexpected status code: {r.status_code}")
+
+    return r.json()
+
+
+try:
+    datasets = get_datasets()
+    print(datasets)
+except Exception as ex:
+    print("ERROR:")
+    print(ex)
