ef1c69aa432789991f52989bfee8fe7ba846b109
Yngvar Kristiansen <yngkri@knowit.no>
2020-09-08 15:24:36 +0200

    #16: ✅ Add /datasets endpoint to latest_provider

--- tools/latest_provider/provider.py
+++ tools/latest_provider/provider.py
@@ -1,13 +1,22 @@
 import json
 import os
-import requests
 import subprocess
 from flask import Flask, make_response, request, Response
-
-app = Flask(__name__)
+from logging.config import dictConfig
 
 DATA_DIR = os.environ['PROVIDER_DATA_DIR']
 DATASERVICE_DIR = os.path.join(DATA_DIR, 'dataservice')
+DATASET_DIR = os.path.join(DATA_DIR, 'dataset')
+
+app = Flask(__name__)
+
+# Configure logging
+dictConfig({
+    'version': 1,
+    'root': {
+        'level': 'INFO'
+    }
+})
 
 def preflight():
     response = make_response()
@@ -15,11 +24,12 @@ def preflight():
 
     return response
 
-def apisToJSONResponse(apis):
-    response = make_response(json.dumps(apis))
+def toJSONResponse(data):
+    response = make_response(json.dumps(data))
     response.mimetype = 'application/json'
 
     return response
+
 def apisToTurtleResponse(apis):
     cmd = ['python', 'scripts/turtle.py']
     process = subprocess.run(
@@ -56,9 +66,49 @@ def get_apis():
     mimetypes = request.headers['Accept'].split(',')
 
     if 'application/json' in mimetypes:
-        response = apisToJSONResponse(apis)
+        response = toJSONResponse(apis)
     elif 'text/turtle' in mimetypes:
         response = apisToTurtleResponse(apis)
+    else:
+        response = make_response(f'unknown mimetype {mimetypes}')
+
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+    return response
+
+def datasetsToTurtleResponse(datasets):
+    cmd = ['python', 'scripts/dataset_turtler.py']
+    process = subprocess.run(
+        cmd,
+        capture_output = True,
+        input = json.dumps(datasets).encode('utf-8')
+    )
+
+    if process.returncode != 0:
+        app.logger.error("Process returned with non-zero exit code")
+        app.logger.error(process.stdout.decode())
+        app.logger.error(process.stderr.decode())
+        process.check_returncode()
+
+    response = make_response(process.stdout.decode())
+    response.mimetype = 'text/turtle'
+
+    return response
+
+@app.route('/datasets', methods=['GET', 'OPTIONS'])
+def get_datasets():
+    if request.method == 'OPTIONS':
+        return preflight()
+
+    with open(os.path.join(DATASET_DIR, '30_result', 'public.json'), 'r') as f:
+        datasets = json.loads(f.read())
+
+    mimetypes = request.headers['Accept'].split(',')
+
+    if 'application/json' in mimetypes:
+        response = toJSONResponse(datasets)
+    elif 'text/turtle' in mimetypes:
+        response = datasetsToTurtleResponse(datasets)
     else:
         response = make_response(f'unknown mimetype {mimetypes}')
 
