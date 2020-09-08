b5a9a103cce46f32840b04ec4dcf4249f565270d
Yngvar Kristiansen <yngkri@knowit.no>
2020-09-08 13:54:08 +0200

    #16: ✅ Add dataset harvester

--- harvest-dataset/dataplatform-harvester/dataplatform-harvester.py
+++ harvest-dataset/dataplatform-harvester/dataplatform-harvester.py
@@ -1,23 +1,42 @@
 import requests
 import os
 import json
+from origo.devportal.poctools.models import Dataset
 
-DATASETS_BASE_URL = os.environ['DATASET_API_BASE_URL']
+DATASET_API_BASE_URL = os.environ['DATASET_API_BASE_URL']
 
 
-def get_datasets():
+def getDatasets():
     headers = {'accept': 'application/json'}
-    r = requests.get(f"{DATASETS_BASE_URL}/metadata/datasets", headers=headers)
+    response = requests.get(f"{DATASET_API_BASE_URL}/metadata/datasets", headers=headers)
 
-    if r.status_code != 200:
-        raise Exception(f"Got unexpected status code: {r.status_code}")
+    if response.status_code != 200:
+        raise Exception(f"Got unexpected status code: {response.status_code}")
 
-    return json.dumps(r.json())
+    return response.json()
 
+# We only care about some fields in the dataset for now, we don't want to store everything.
+def convertToOurDomainModel(datasets):
+    result = []
+    for dataset in datasets:
+        stripped = Dataset()
+        stripped.identifier = dataset['Id']
+        stripped.title = dataset['title']
+        stripped.description = dataset.get('description', '')
+        stripped.publisher = dataset.get('publisher', '')
+
+        result.append(stripped)
+
+    return result
+
+def toJson(datasets):
+    return json.dumps([dataset.serialize() for dataset in datasets])
 
 try:
-    datasets = get_datasets()
-    print(datasets)
+   input_datasets = getDatasets()
+   output_datasets = convertToOurDomainModel(input_datasets)
+
+   print(toJson(output_datasets))
 except Exception as ex:
-    print("ERROR:")
-    print(ex)
+   print("An error occurred when harvesting.")
+   raise ex
