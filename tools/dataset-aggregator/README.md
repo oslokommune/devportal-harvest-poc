70a0fbc832eb6435e73b235534c8f83ff834d439
Yngvar Kristiansen <yngvar.kristiansen@byr.oslo.kommune.no>
2020-09-09 10:58:04 +0200

    #16: ✅ Add Dataset handling (#21)

---
+++ tools/dataset-aggregator/README.md
@@ -0,0 +1,29 @@
+# Dataset harvest aggregator
+
+## What
+
+This service adds visibility to previously harvested data sets.
+
+### visibility.json
+
+The input to this service is datasets to stdin and the file visibility.json. visibility.json is a file that specifies
+the visibility level of a dataset with a given identifier.
+
+```json
+{
+	"<original name>": "<visibility level>",
+	"example-dataset": "PUBLIC",
+	"private-example-dataset": "PROTECTED",
+	...
+}
+```
+
+This determines if the dataset should end up in the `public.json` file or the
+`protected.json` file. If the dataset is not found in `visibility.json`, it's hidden
+from both files.
+
+`public.json` is exposed by latest_provider as json and turtle. It is also used
+by the distributors.
+
+`protected.json` will be exposed to authorized users when authorization is
+implemented.
--- tools/latest_provider/provider.py
+++ tools/latest_provider/provider.py
@@ -1,13 +1,23 @@
 import json
 import os
-import requests
 import subprocess
 from flask import Flask, make_response, request, Response
-
-app = Flask(__name__)
+from logging.config import dictConfig
 
 DATA_DIR = os.environ['PROVIDER_DATA_DIR']
 DATASERVICE_DIR = os.path.join(DATA_DIR, 'dataservice')
+DATASET_DIR = os.path.join(DATA_DIR, 'dataset')
+
+app = Flask(__name__)
+
+# Configure logging
+dictConfig({
+    'version': 1,
+    'root': {
+        'level': 'INFO'
+    }
+})
+
 
 def preflight():
     response = make_response()
@@ -15,23 +25,27 @@ def preflight():
 
     return response
 
-def apisToJSONResponse(apis):
-    response = make_response(json.dumps(apis))
+
+def toJSONResponse(data):
+    response = make_response(json.dumps(data))
     response.mimetype = 'application/json'
 
     return response
+
+
 def apisToTurtleResponse(apis):
     cmd = ['python', 'scripts/turtle.py']
     process = subprocess.run(
         cmd,
-        capture_output = True,
-        input = json.dumps(apis).encode('utf-8')
+        capture_output=True,
+        input=json.dumps(apis).encode('utf-8')
     )
 
     response = make_response(process.stdout.decode())
     response.mimetype = 'text/turtle'
 
     return response
+
 
 @app.route('/apis', methods=['GET', 'OPTIONS'])
 def get_apis():
@@ -56,9 +70,59 @@ def get_apis():
     mimetypes = request.headers['Accept'].split(',')
 
     if 'application/json' in mimetypes:
-        response = apisToJSONResponse(apis)
+        response = toJSONResponse(apis)
     elif 'text/turtle' in mimetypes:
         response = apisToTurtleResponse(apis)
+    else:
+        response = make_response(f'unknown mimetype {mimetypes}')
+
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+    return response
+
+
+def datasetsToTurtleResponse(datasets):
+    cmd = ['python', 'scripts/dataset_turtler.py']
+    process = subprocess.run(
+        cmd,
+        capture_output=True,
+        input=json.dumps(datasets).encode('utf-8')
+    )
+
+    if process.returncode != 0:
+        app.logger.error("Process returned with non-zero exit code")
+        app.logger.error(process.stdout.decode())
+        app.logger.error(process.stderr.decode())
+        process.check_returncode()
+
+    response = make_response(process.stdout.decode())
+    response.mimetype = 'text/turtle'
+
+    return response
+
+
+# Output shouldn't contain visibility for now.
+def remove_visibility(datasets):
+    for d in datasets:
+        d.pop("visibility", None)
+
+
+@app.route('/datasets', methods=['GET', 'OPTIONS'])
+def get_datasets():
+    if request.method == 'OPTIONS':
+        return preflight()
+
+    with open(os.path.join(DATASET_DIR, '30_result', 'public.json'), 'r') as f:
+        datasets = json.loads(f.read())
+
+    remove_visibility(datasets)
+
+    mimetypes = request.headers['Accept'].split(',')
+
+    if 'application/json' in mimetypes:
+        response = toJSONResponse(datasets)
+    elif 'text/turtle' in mimetypes:
+        response = datasetsToTurtleResponse(datasets)
     else:
         response = make_response(f'unknown mimetype {mimetypes}')
 
